2a1bf0d74be24076890435785041401b8db54519
Xavier Via <fernando.via@gmail.com>
2016-02-05 22:34:15 +0000

    Added a different, even cleaner approach. Tests pending

--- example/src/actions/random.js
+++ example/src/actions/random.js
@@ -1,14 +1,8 @@
-import * as RandomWalks from '../walks/random'
-
 export const randomNumberInRandomTime = function () {
   const numberOfMilliseconds = Math.floor(Math.random() * 5000)
   return {
     type: 'RANDOM_NUMBER_IN_RANDOM_TIME',
-    payload: numberOfMilliseconds,
-    walk: {
-      through: RandomWalks.callBackInX(numberOfMilliseconds),
-      to: randomNumberInRandomTimeReceived
-    }
+    payload: numberOfMilliseconds
   }
 }
 
