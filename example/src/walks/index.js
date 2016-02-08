3240b6161ae7e7f3539a728f40f60030b15b8c3f
Xavier Via <fernando.via@gmail.com>
2016-02-08 16:14:03 +0000

    Tentative update of the examplTentative update of the examplee

--- example/src/walks/index.js
+++ example/src/walks/index.js
@@ -1,11 +1,13 @@
 import * as RandomActions from '../actions/random'
 
-export default function (state, { type, payload }) {
+export default function (result, { type, payload }) {
   switch (type) {
     case 'RANDOM_NUMBER_IN_RANDOM_TIME':
-      return {
-        through: (done) => setTimeout(() => done(payload / 1000), payload),
-        to: RandomActions.randomNumberInRandomTimeReceived
-      }
+      return RandomActions.randomNumberInRandomTimeReceived
+    // case 'RANDOM_NUMBER_IN_RANDOM_TIME':
+    //   return {
+    //     resolved: RandomActions.randomNumberInRandomTimeReceived,
+    //     rejected: RandomActions.randomNumberInRandomTimeReceivedFailure
+    //   }
   }
 }
